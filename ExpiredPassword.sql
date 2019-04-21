1ee6a06998ba5060468dfd1d3971c9bb334cf8a3
Aziz Prastyo Wibowo <wibowo.azizprastyo@gmail.com>
2019-04-21 21:03:01 +0700

    Commit Delete 20190421

--- ExpiredPassword.sql
+++
@@ -1,3 +0,0 @@
-Unlimited Expired Password
---------------------------
-ALTER PROFILE "DEFAULT" LIMIT PASSWORD_LIFE_TIME UNLIMITED;
--- Export_dump_mysql.sql
+++
@@ -1,1 +0,0 @@
-SELECT nama-kolom FROM nama-tabel INTO OUTFILE '/tmp/nama-file.txt' FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n';
