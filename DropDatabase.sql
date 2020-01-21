666e7bb109dab02bc5ace38539784245a0626bec
Aziz Prastyo Wibowo <AzizWi@metrodata.co.id>
2020-01-21 14:00:06 +0700

    AzizPW Commit 20200121

--- DropDatabase.sql
+++ DropDatabase.sql
@@ -1,2 +1,4 @@
-STARTUP MOUNT EXCLUSIVE RESTRICT;
-DROP DATABASE;
+Please set parameter cluster_database=false in RAC environment before do this script.
+
+SQL> STARTUP MOUNT EXCLUSIVE RESTRICT;
+SQL> DROP DATABASE;
